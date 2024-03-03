859d74de406fc66326f99ee6e62a0f224e0b4e7d
Meghavati Chaudhari <meghapjc29@gmail.com>
2024-03-03 23:44:57 +0530

    todo using context api

---
+++ project8/todo using contextapi local storage/src/contexts/TodoContext.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+import { createContext } from 'react';
+
+export const TodoContext=createContext({ todos:[{id:1,todo:"todo msg",completed:false,}], addTodo:(todo)=>{},updatedTodo:(id,todo)=>{},deleteTodo:(id,todo)=>{},toggleComplete:(id)=>{}})
+
+export const useTodo=()=>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider=TodoContext.Provider;
+
